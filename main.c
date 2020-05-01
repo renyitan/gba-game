732b7f28468d143854d2f7b665814f1147def484
renyitan <tanrenyi@gmail.com>
2020-05-01 02:52:50 -0400

    white background

--- main.c
+++ main.c
@@ -1,65 +1,47 @@
 // -----------------------------------------------------------------------------
 // C-Skeleton to be used with HAM Library from www.ngine.de
 // -----------------------------------------------------------------------------
-#include "numbers.h"
-#include "gba.h"
-#include "mygbalib.h"
+// #include "numbers.h"
+// #include "gba.h"
+// #include "mygbalib.h"
 
-// Global variable for counter
-int COUNTER_NUM = 0;
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
 
-void Handler(void)
-{
-    REG_IME = 0x00; // Stop all other interrupt handling, while we handle this current one
-    
-    if ((REG_IF & INT_TIMER0) == INT_TIMER0) // handling INT_TIMER0 interrupt
-    {
-        // screen width/2 -4 is for offset of the sprites to centralise them
+#define SCREENBUFFER ((volatile uint16 *)0x06000000)
+#define REG_DISPLAYCONTROL *((volatile uint32 *)(0x04000000))
+#define VIDEOMODE_3 0x0003
+#define BGMODE_2 0x0400
 
-        drawSprite(((COUNTER_NUM / 10) % 10), 1, SCREEN_WIDTH/2-4, SCREEN_HEIGHT/2);  // sprite for the ten's digit positions
-        drawSprite((COUNTER_NUM % 10), 0, SCREEN_WIDTH/2+4, SCREEN_HEIGHT/2); // sprite for the one's digit positions
-        COUNTER_NUM++;
-    }
-    
-    REG_IF = REG_IF; // Update interrupt table, to confirm we have handled this interrupt
-    
-    REG_IME = 0x01;  // Re-enable interrupt handling
-}
-
+#define SCREENBUFFER ((volatile uint16 *)0x06000000)
+#define SCREEN_W 240
+#define SCREEN_H 160
 
 // -----------------------------------------------------------------------------
 // Project Entry Point
 // -----------------------------------------------------------------------------
 int main(void)
 {
-	int i;
-	
-    // Set Mode 2
-    *(unsigned short *) 0x4000000 = 0x40 | 0x2 | 0x1000;
+    // Set Mode 3
+    REG_DISPLAYCONTROL = VIDEOMODE_3 | BGMODE_2;
 
     // Fill SpritePal
-    *(unsigned short *) 0x5000200 = 0;
-    *(unsigned short *) 0x5000202 = RGB(31,31,31);
+    // *(unsigned short *)0x5000200 = 0;
+    // *(unsigned short *)0x5000202 = RGB(31, 31, 31);
 
     // Fill SpriteData
-    for (i = 0; i < 10*8*8/2; i++)
-        spriteData[i] = (numbers[i*2+1] << 8) + numbers[i*2];
+    // for (i = 0; i < 10 * 8 * 8 / 2; i++)
+    //     spriteData[i] = (numbers[i * 2 + 1] << 8) + numbers[i * 2];
 
-    // Set Handler Function for interrupts and enable selected interrupts
-    REG_INT = (int)&Handler;
-    REG_IE |= INT_TIMER0;	// choose which interrupt to enable.S
-    REG_IME = 0x1;		// Enable interrupt handling
+    int i;
+    for (i = 0; i < SCREEN_W * SCREEN_H; ++i)
+    {
+        SCREENBUFFER[i] = 0xFFFF;
+    }
 
-    // Set Timer Mode (fill that section and replace TMX with selected timer number)
-    REG_TM0D =	-0x4000;		// initial counter value of -16384, return to this value when counter overflows.
-    // REG_TM0D = 0;
-    REG_TM0CNT |= TIMER_FREQUENCY_256 | TIMER_INTERRUPTS | TIMER_ENABLE; 	
+    while (1)
+        ;
 
-    // reset to 0 once counter reaches 99
-    if (COUNTER_NUM >= 100) {COUNTER_NUM = 0;}
-
-    while(1);
-
-	return 0;
+    return 0;
 }
-
